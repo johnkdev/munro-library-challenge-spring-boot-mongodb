48571bb3787df6098246dabc8d19924dc420556b
John Kelly <johnkdevelop@gmail.com>
2021-02-06 13:39:17 +0000

    Define package structure

---
+++ src/main/java/com/example/munrolibrarychallenge/MunroLibraryChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.munrolibrarychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunroLibraryChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunroLibraryChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/munrolibrarychallenge/MunroLibraryChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.munrolibrarychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunroLibraryChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
